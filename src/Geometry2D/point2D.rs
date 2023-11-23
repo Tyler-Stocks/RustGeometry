c566357fa7a68ad272cd04d22e60a2de8d717d5a
Tyler-Stocks <tylerjstocks@gmail.com>
2023-11-23 06:01:50 +0000

    Added AddAssign Support for Point2D<i128> and Point2D<f64>

--- src/Geometry2D/point2D.rs
+++ src/Geometry2D/point2D.rs
@@ -6,44 +6,9 @@ pub struct Point2D<T> {
     pub y: T
 }
 
-pub trait Mirror {
-    fn mirror_x(&mut self) -> ();
-    fn mirror_y(&mut self) -> ();
-}
-
-impl Mirror for Point2D<i128> {
-    fn mirror_x(&mut self) -> () { self.x *= -1; }
-    fn mirror_y(&mut self) -> () { self.y *= -1; }
-}
-
-impl Mirror for Point2D<f64> {
-    fn mirror_x(&mut self) -> () { self.x *= -1.0; }
-    fn mirror_y(&mut self) -> () { self.y *= -1.0; }
-}
-
-pub trait Translate<T> {
-    fn translate_x(&mut self, distance: T) -> ();
-    fn translate_y(&mut self, distance: T) -> ();
-    fn tarnslate(&mut self, x_distance: T, y_distance: T) -> ();
-}
-
-impl Translate<i128> for Point2D<i128> {
-    fn translate_x(&mut self, distance: i128) -> () { self.x += distance; }
-    fn translate_y(&mut self, distance: i128) -> () { self.y += distance; }
-    fn tarnslate(&mut self, x_distance: i128, y_distance: i128) -> () {
-        self.x += x_distance;
-        self.y += y_distance;
-    }
-}
-
-impl Translate<f64> for Point2D<f64> {
-    fn translate_x(&mut self, distance: f64) -> () { self.x += distance; }
-    fn translate_y(&mut self, distance: f64) -> () { self.y += distance; }
-    fn tarnslate(&mut self, x_distance: f64, y_distance: f64) -> () {
-        self.x += x_distance;
-        self.y += y_distance;
-    }
-}
+// ----------
+// STD Traits
+// ----------
 
 impl Add for Point2D<f64> {
     type Output = Point2D<f64>;
@@ -109,8 +74,59 @@ impl Div for Point2D<i128> {
     }
 }
 
+impl AddAssign for Point2D<f64> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x = self.x + rhs.x;
+        self.y = self.y + rhs.y;
+    }
+}
+
+impl AddAssign for Point2D<i128> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x = self.x + rhs.x;
+        self.y = self.y + rhs.y;
+    }
+}
+
+// -------------
+// CUSTOM TRAITS
+// -------------
 
 pub trait Reflect {
     fn reflect_x(&mut self) -> ();
-    fn reflect_Y((&mut self))
+    fn reflect_y(&mut self) -> ();
+}
+
+impl Reflect for Point2D<i128> {
+    fn reflect_x(&mut self) -> () { self.x *= -1; }
+    fn reflect_y(&mut self) -> () { self.y *= -1; }
+}
+
+impl Reflect for Point2D<f64> {
+    fn reflect_x(&mut self) -> () { self.x *= -1.0; }
+    fn reflect_y(&mut self) -> () { self.y *= -1.0; }
+}
+
+pub trait Translate<T> {
+    fn translate_x(&mut self, distance: T) -> ();
+    fn translate_y(&mut self, distance: T) -> ();
+    fn tarnslate(&mut self, x_distance: T, y_distance: T) -> ();
+}
+
+impl Translate<i128> for Point2D<i128> {
+    fn translate_x(&mut self, distance: i128) -> () { self.x += distance; }
+    fn translate_y(&mut self, distance: i128) -> () { self.y += distance; }
+    fn tarnslate(&mut self, x_distance: i128, y_distance: i128) -> () {
+        self.x += x_distance;
+        self.y += y_distance;
+    }
+}
+
+impl Translate<f64> for Point2D<f64> {
+    fn translate_x(&mut self, distance: f64) -> () { self.x += distance; }
+    fn translate_y(&mut self, distance: f64) -> () { self.y += distance; }
+    fn tarnslate(&mut self, x_distance: f64, y_distance: f64) -> () {
+        self.x += x_distance;
+        self.y += y_distance;
+    }
 }
